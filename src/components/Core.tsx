49098aa0c431c9340d28d76c820b86b30c17e19e
Sarah Pallamidessi <sarah@saascada.com>
2020-11-25 16:31:00 +0000

    grid view on sheets

--- src/components/Core.tsx
+++ src/components/Core.tsx
@@ -1,12 +1,14 @@
 import React, { Component } from 'react'
 import Sidebar from './Sidebar'
 import styles from '../styles/core.module.css'
+import Sheets from './Sheets'
 
 export default class Core extends Component {
     render() {
         return (
             <div className={styles.container}>
                 <Sidebar/>
+                <Sheets/>
             </div>
         )
     }
